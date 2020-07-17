807141a2193dfce71378219128edf8c4b9f95bfd
John Fredy Ríos <49043840+fredyr01@users.noreply.github.com>
2020-07-16 22:35:14 -0500

    Add files via upload
    Documentación necesaria para la página canina

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector('.hamburger');
+
+const menu = document.querySelector('.menu-navegacion');
+
+
+
+
+// Quita y pone la clase spread
+hamburger.addEventListener('click', ()=>{
+    menu.classList.toggle("spread");
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburger){
+        
+        menu.classList.toggle("spread");
+
+
+    }
+})
+
+
